1545e42b69994451bd22c430ad8108f51c9e9925
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-11 14:38:30 +0300

    APP

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,15 @@ import { Route, Routes } from 'react-router-dom'
 import { useAuth } from './Context/AuthContext'
 import Landing from './Pages/Landing';
 import Dashboard from './Pages/User/Dashboard';
+import Nav from './Components/Nav';
 
 export const App = () => {
   const {user} = useAuth();
   return (
     <div>
-
+      {
+        user == null && <Nav/>  
+      }
       <Routes>
         <Route path='/' element={user != null ? <Dashboard/> : <Landing/> }>
         </Route>
