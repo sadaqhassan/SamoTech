059ed906d9c9da78e8d2274aba56483391a4cdd9
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-11 14:38:40 +0300

    AUTH

--- client/src/Pages/Auth.jsx
+++ client/src/Pages/Auth.jsx
@@ -22,7 +22,7 @@ const Auth = () => {
         {/* Title */}
         <div>
           <h1 className="text-3xl font-bold text-white">
-            {state === "login" ? "Welcome Back 👋" : "Create Account 🚀"}
+            {state === "login" ? "Welcome Back " : "Create Account "}
           </h1>
 
           <p className="text-gray-400 mt-2 text-sm">
