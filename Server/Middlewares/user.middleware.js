bb530cbcf17c4d632bdf676bba0ebb5c84abfb8f
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-11 15:06:09 +0300

    useRMiddl

---
+++ Server/Middlewares/user.middleware.js
@@ -0,0 +1,14 @@
+//is auth
+export const isAuth = (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if(!token){
+            return res.status(401).json({message:"Unauthorized"});
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.id;
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
