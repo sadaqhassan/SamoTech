3ff35155d6511592a50e8d2572cb6424854202c8
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-11 15:08:21 +0300

    userOR

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,16 @@
 import express from 'express'
 import { configApp } from './configs/configs';
+import { CONNDB } from './configs/db.config.js';
+import { userRoute } from './Routes/user.route.js';
 
 const app = express();
 const PORT = process.env.PORT 
 
+app.use('/api/user', userRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 CONNDB();
 
