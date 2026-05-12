519414f56bff64046ccae0c03faa71a64dd491b0
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-12 10:25:46 +0300

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import { useAuth } from './Context/AuthContext'
 import Landing from './Pages/Landing';
 import Dashboard from './Pages/User/Dashboard';
 import Nav from './Components/Nav';
+import { Toaster } from 'react-hot-toast';
 
 export const App = () => {
   const {user} = useAuth();
   return (
     <div>
+      {<Toaster/>}
       {
         user == null && <Nav/>  
       }
