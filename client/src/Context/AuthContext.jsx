760b29a75ed65aeb508488b3c7333abc08ac0524
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-12 10:25:57 +0300

    userData haandle

--- client/src/Context/AuthContext.jsx
+++ client/src/Context/AuthContext.jsx
@@ -5,22 +5,43 @@ export const AuthContext = createContext();
 
 export const AuthContextProvider = ({children})=>{
 
+    const BackendApi =  "http://localhost:4000/api"; 
+    
     const [user,setUser] = useState(()=>{
         const getUser = localStorage.getItem("user");
         
         return getUser ? JSON.parse(getUser) : null
     });
     
+    const [isLogedIn,setIsLoggedIn] = useState(false);
+
     useEffect(()=>{
         if(user){
             localStorage.setItem("user",JSON.stringify(user));
         }else{
             localStorage.removeItem("user");
         }
-    });
+    },[user]);
+
+    // const fetchUser = async()=>{
+    //             const res = await fetch(`${BackendApi}/user/get-user`,{
+    //                 method: "GET",
+    //                 headers: {
+    //                     "content-type": "application/json",
+    //                 },
+    //                 credentials: "include"
+    //                 })
+    //                 const data = await res.json();
+    //                 if(data.success){
+    //                     setUser(data.userData);
+    //                 }else{
+    //                     setUser(null);
+    //                 }
+    //             }
 
     const value = {
-        user,setUser
+        user,setUser,
+        // isLogedIn,setIsLoggedIn,fetchUser
     };
 
     return <AuthContext value={value}>
