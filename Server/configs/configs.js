9b1fc72b7840865ea5bc01f9b0cc2cd666507973
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-05-12 10:26:28 +0300

    configs

--- Server/configs/configs.js
+++ Server/configs/configs.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 export const configApp = (app)=>{
     dotenv.config();
